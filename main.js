593aedcee7f685be631674c99e231f3f0ab4744d
VoitMax <maximilian.voit@student.uibk.ac.at>
2022-06-02 17:05:00 +0200

    Wettervorhersage...

--- main.js
+++ main.js
@@ -56,7 +56,16 @@ async function loadWind(url) {
     layerControl.addOverlay(overlays.wind, `ECMWF Windvorhersage für ${forecastLabel}`)
 
     L.velocityLayer({
-        data: jsondata
+        data: jsondata,
+        lineWidth: 2,
+        displayOptions: {
+            velocityType: "",
+            directionString: "Windrichtung",
+            speedString: "Windgeschwindigkeit",
+            speedUnit: "km/h",
+            emptyString: "keine Daten vorhanden",
+            position: "bottomright"
+        }
     }).addTo(overlays.wind);
 
 
@@ -66,6 +75,9 @@ loadWind("https://geographie.uibk.ac.at/webmapping/ecmwf/data/wind-10u-10v-europ
 
 // Wettervorhersage
 async function loadWeather(url) {
+    const response = await fetch(url);
+    const jsondata = await response.json();
+    console.log("jsondaten", jsondata)
 
 };
 loadWeather("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=47.267222&lon=11.392778");
